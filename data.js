55354a2d107c79e669d6d5cd5f1940fbf7b96a50
Monique Legaspi <mlegaspi@seas.upenn.edu>
2023-07-02 23:40:10 -0400

    separate data file

---
+++ data.js
@@ -0,0 +1,12 @@
+const weekday = ["sunday","monday","tuesday","wednesday","thursday","friday","saturday"];
+const months = ["january", "february", "march", "april", "may", "june", "july", "august", "september", "october", "november", "december"];
+
+const libHours = {
+    "biotech": [[10,18],[8,23],[8,23],[8,23],[8,23],[8,22],"x"],
+    "fisher": ["x",[9,18],[9,18],[9,18],[9,18],[9,17],"x"],
+    "huntsman": [[7,22],[7,22],[7,22],[7,22],[7,22],[7,22],[7,22]],
+    "tangen": [[7,22],[7,22],[7,22],[7,22],[7,22],[7,22],[7,22]],
+    "vp": [[10,18],[9,18],[9,18],[9,18],[9,18],[9,18],[10,18]],
+
+    "pottruck":[[9,20],[6,22],[6,22],[6,22],[6,22],[6,22],[8,20]]
+}
--- script.js
+++ script.js
@@ -7,8 +7,6 @@ console.log(datetime);
 // refresh datetime appropriately
 // and change ticker location
 var tickers = document.getElementsByClassName("ticker");
-const weekday = ["sunday","monday","tuesday","wednesday","thursday","friday","saturday"];
-const months = ["january", "february", "march", "april", "may", "june", "july", "august", "september", "october", "november", "december"];
 function refreshTime() {
     const timeDisplay = document.getElementById("time");
 
@@ -49,15 +47,7 @@ setInterval(refreshTime, 1000);
 /***** CALENDAR FUNCTIONALITY *****/
 
 // check whether open at that time
-const libHours = {
-    "biotech": [[10,18],[8,23],[8,23],[8,23],[8,23],[8,22],"x"],
-    "fisher": ["x",[9,18],[9,18],[9,18],[9,18],[9,17],"x"],
-    "huntsman": [[7,22],[7,22],[7,22],[7,22],[7,22],[7,22],[7,22]],
-    "tangen": [[7,22],[7,22],[7,22],[7,22],[7,22],[7,22],[7,22]],
-    "vp": [[10,18],[9,18],[9,18],[9,18],[9,18],[9,18],[10,18]],
 
-    "pottruck":[[9,20],[6,22],[6,22],[6,22],[6,22],[6,22],[8,20]]
-}
 function isOpen(colId, hr) {
     var col = document.getElementById(colId);
 
